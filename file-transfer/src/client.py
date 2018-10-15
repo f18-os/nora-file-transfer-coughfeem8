f12a153d9511bf8af59796e41f0e9248ee978910
Sergio Yanez <seryan.1115@gmail.com>
2018-10-15 16:47:00 -0600

    one more git ignore

---
+++ file-transfer/src/client.py
@@ -0,0 +1,71 @@
+import sys
+sys.path.append('../test')
+sys,=.path.append('../emphaticDemo')
+import logSetup as log                              # my loggin module
+
+
+switchesVarDefaults = (
+    (('-s', '--server'), 'server', "localhost:50001"),
+    (('-d', '--debug'), "debug", False), # boolean (set if present)
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+
+progname = "framedClient"
+paramMap = params.parseParams(switchesVarDefaults)
+
+server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
+
+if usage:
+    params.usage()
+
+
+try:
+    serverHost, serverPort = re.split(":", server)
+    serverPort = int(serverPort)
+except:
+    print("Can't parse server:port from '%s'" % server)
+    sys.exit(1)
+
+class ClientThread(Thread):
+    def __init__(self, serverHost, serverPort, debug):
+        Thread.__init__(self, daemon=False)
+        self.serverHost, self.serverPort, self.debug = serverHost, serverPort, debug
+        self.start()
+    def run(self):
+       s = None
+       for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
+           af, socktype, proto, canonname, sa = res
+           try:
+               print("creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto))
+               s = socket.socket(af, socktype, proto)
+           except socket.error as msg:
+               print(" error: %s" % msg)
+               s = None
+               continue
+           try:
+               print(" attempting to connect to %s" % repr(sa))
+               s.connect(sa)
+           except socket.error as msg:
+               print(" error: %s" % msg)
+               s.close()
+               s = None
+               continue
+           break
+
+       if s is None:
+           print('could not open socket')
+           sys.exit(1)
+
+       fs = FramedStreamSock(s, debug=debug)
+
+
+       print("sending hello world")
+       fs.sendmsg(b"hello world")
+       print("received:", fs.receivemsg())
+
+       fs.sendmsg(b"hello world")
+       print("received:", fs.receivemsg())
+
+for i in range(100):
+    ClientThread(serverHost, serverPort, debug)
